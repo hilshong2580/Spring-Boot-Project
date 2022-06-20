ffca65e69f923652494d03a938de79f8b37daef5
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2022-06-20 17:26:02 +0800

    The Control layer that responsible for processing incoming REST API requests, preparing a model, and returning the view to be rendered as a response.

--- src/main/java/com/example/Shing_practice/student/StudentController.java
+++ src/main/java/com/example/Shing_practice/student/StudentController.java
@@ -1,14 +1,10 @@
 package com.example.Shing_practice.student;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
-
 
 /*
  * The @RestController can default a rest end point
@@ -35,5 +31,23 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+    @DeleteMapping( path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
     }
 }
